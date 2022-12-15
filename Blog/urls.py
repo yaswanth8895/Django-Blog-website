1e61100482964ba0efcbff8a53a84364bc72f310
yaswanth8895 <85066701+yaswanth8895@users.noreply.github.com>
2022-12-15 18:38:08 +0530

    second commit

--- Blog/urls.py
+++ Blog/urls.py
@@ -20,6 +20,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
 from articles import views as article_views
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +29,8 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('about/', views.about),
     path('', article_views.article_list, name="home"),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 urlpatterns += staticfiles_urlpatterns()
